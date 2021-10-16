4537c2be204bb68263ec11b2b49839ec34aa99eb
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-17 00:19:42 +0530

    added daily wage computation program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,25 +1,31 @@
 package com.bridgelabz.oops;
 
-public class EmployeeWageComputation{
-	
+public class EmployeeWageComputation {
 
-	static int IS_PRESENT = 1;
-	
-//	getting 0 & 1 random value
-	static double RANDOM_CHECK = Math.floor(Math.random() * 10) % 2;
-	
-//	creating function to check Employee Attendence
-	static void empCheck() {
-		if (IS_PRESENT == RANDOM_CHECK) {
-			System.out.println("Employee Is present");
-		}
-		else {
-			System.out.println("Employee is absent");
-		}
-	}
-	
 	public static void main(String[] args) {
-//		calling function
-		empCheck();
+	
+		EmployeeWageComputation e = new EmployeeWageComputation();
+		e.employeeDailyWage();
+
+	}
+//	constants
+	static int IS_PRESENT = 1;
+	static int EMP_WAGE_PER_HR = 20;
+	static int WORK_HRS = 8;
+	
+//	generating random value (0 &1)
+	double EMP_CHECK = Math.floor(Math.random() * 10) % 2;
+	int empHrs = 0;
+	
+//	creating method to calculate daily wage
+	public void employeeDailyWage() {
+		
+		if (IS_PRESENT == EMP_CHECK) {
+			empHrs=8;
+		}else {
+			empHrs = 0;
+		}
+		int empWage = EMP_WAGE_PER_HR * empHrs;
+		System.out.println("Daily Employee Wage is " + empWage);
 	}
 }
