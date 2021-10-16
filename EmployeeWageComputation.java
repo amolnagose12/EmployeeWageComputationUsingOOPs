1a1e77b2cb412e3c67528db5073f2810124ebaa3
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-17 00:26:08 +0530

    Added full time wage and paart time wage program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,26 +3,29 @@ package com.bridgelabz.oops;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-	
+//		creating object
 		EmployeeWageComputation e = new EmployeeWageComputation();
 		e.employeeDailyWage();
-
 	}
 //	constants
-	static int IS_PRESENT = 1;
+	static int IS_FULLTIME = 1;
+	static int IS_PART_TIME = 2;
 	static int EMP_WAGE_PER_HR = 20;
-	static int WORK_HRS = 8;
+
 	
 //	generating random value (0 &1)
-	double EMP_CHECK = Math.floor(Math.random() * 10) % 2;
+	double EMP_CHECK = Math.floor(Math.random() * 10) % 3;
 	int empHrs = 0;
 	
 //	creating method to calculate daily wage
 	public void employeeDailyWage() {
 		
-		if (IS_PRESENT == EMP_CHECK) {
-			empHrs=8;
-		}else {
+		if (IS_FULLTIME == EMP_CHECK) {
+			empHrs=8;	
+		}else if (IS_PART_TIME == EMP_CHECK) {
+			empHrs = 4;
+		}
+		else {
 			empHrs = 0;
 		}
 		int empWage = EMP_WAGE_PER_HR * empHrs;
