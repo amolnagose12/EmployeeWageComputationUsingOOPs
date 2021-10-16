62280d5bce09f26e20c704f602794e62c1bd6c53
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-17 00:31:29 +0530

    Added part time full time wage program using switch case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,34 +1,39 @@
 package com.bridgelabz.oops;
 
 public class EmployeeWageComputation {
+	
+//	CONSTANTS
+	
+	static int IS_FULLTIME = 1;
+	static int IS_PART_TIME = 2;
+	static int IS_ABSENT = 0;
+	static int EMP_WAGE_PER_HR = 20;
+	
+//	generating random number(0,1,2)
+	
+	double  emp_Check = Math.floor(Math.random() * 10) % 3;
+	int EMP_CHECK = (int) emp_Check;
+	static int empHrs = 0;
+	
+	public void empCheck() {
+		switch(EMP_CHECK) {
+		case (1):
+			empHrs = 8;
+			break;
+		case (2):
+			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+		}
+	int empWage = EMP_WAGE_PER_HR * empHrs;
+	System.out.println("Daily Employee Wage is " + empWage);
+	}
 
+	
 	public static void main(String[] args) {
 //		creating object
 		EmployeeWageComputation e = new EmployeeWageComputation();
-		e.employeeDailyWage();
-	}
-//	constants
-	static int IS_FULLTIME = 1;
-	static int IS_PART_TIME = 2;
-	static int EMP_WAGE_PER_HR = 20;
-
-	
-//	generating random value (0 &1)
-	double EMP_CHECK = Math.floor(Math.random() * 10) % 3;
-	int empHrs = 0;
-	
-//	creating method to calculate daily wage
-	public void employeeDailyWage() {
-		
-		if (IS_FULLTIME == EMP_CHECK) {
-			empHrs=8;	
-		}else if (IS_PART_TIME == EMP_CHECK) {
-			empHrs = 4;
-		}
-		else {
-			empHrs = 0;
-		}
-		int empWage = EMP_WAGE_PER_HR * empHrs;
-		System.out.println("Daily Employee Wage is " + empWage);
+		e.empCheck();
 	}
 }
