3fffb26ce0605bd490e525c32e1e664c8500cfd2
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-17 00:36:10 +0530

    Added employee wage per month programm

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,39 +1,37 @@
 package com.bridgelabz.oops;
 
 public class EmployeeWageComputation {
-	
-//	CONSTANTS
-	
-	static int IS_FULLTIME = 1;
-	static int IS_PART_TIME = 2;
-	static int IS_ABSENT = 0;
-	static int EMP_WAGE_PER_HR = 20;
-	
-//	generating random number(0,1,2)
-	
-	double  emp_Check = Math.floor(Math.random() * 10) % 3;
-	int EMP_CHECK = (int) emp_Check;
-	static int empHrs = 0;
-	
-	public void empCheck() {
-		switch(EMP_CHECK) {
-		case (1):
-			empHrs = 8;
-			break;
-		case (2):
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
-		}
-	int empWage = EMP_WAGE_PER_HR * empHrs;
-	System.out.println("Daily Employee Wage is " + empWage);
-	}
 
-	
 	public static void main(String[] args) {
-//		creating object
 		EmployeeWageComputation e = new EmployeeWageComputation();
 		e.empCheck();
+	}
+	
+	public void empCheck() {
+//		constants
+		
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_WAGE_PER_HR = 20;
+		int NO_OF_WORKING_DAYS = 20;
+		int empHrs = 0;
+//		GENERATING VALUES 0,1,2 
+		double EMP_CHECK = Math.floor(Math.random() * 10) % 3;
+		
+//		computing emp wage for 20days
+		for (int day = 1; day <= NO_OF_WORKING_DAYS; day++) {
+			
+			if (IS_FULL_TIME == EMP_CHECK) {
+				empHrs=8;
+				
+			}else if (IS_PART_TIME == EMP_CHECK) {
+				empHrs = 4;	
+			}
+			else {
+				empHrs = 0;
+			}
+		}
+		int empWage = EMP_WAGE_PER_HR * empHrs;
+		System.out.println("Daily Employee Wage is " + empWage);
 	}
 }
