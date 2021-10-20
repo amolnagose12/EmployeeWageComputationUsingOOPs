731499466a1384eaebc2a9bf92934339d9721bd6
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-20 15:38:21 +0530

    Added employee Wage for multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,46 +1,48 @@
-package com.bridgelabz.oops;
+package com.bridgelabz;
 
-public class EmployeeWageComputation {
+public class EmployeeWageComputation{
+	
+//	constants
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	
+	public void computeWage(String cmpName, int empRatePerHr, int numOfWorkingDays, int maxHrsInMonth){
 
-	public static void main(String[] args) {
-		EmployeeWageComputation e = new EmployeeWageComputation();
-		e.empCheck();
+//		initializing variable
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+//computation
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays){
 
-	}
-	public void empCheck() {
-	final int IS_PART_TIME = 1;
-	final int IS_FULL_TIME = 2;
-	final int EMP_RATE_PER_HR = 20;
-	final int NO_OF_WORKING_DAYS = 20;
-	final int MAX_HR_PER_MONTH = 100;
+			totalWorkingDays++;
 
-		// initializing variable
-	int empHrs = 0;
-	int	totalEmpWage = 0;
-	int	totalEmpHrs = 0;
-	int	totalWorkingDays = 0;
-		// computation
-		
-	while (totalEmpHrs <= MAX_HR_PER_MONTH && totalWorkingDays <NO_OF_WORKING_DAYS) {
-		totalWorkingDays++;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
+			int empCheck = (int) Math.floor(Math.random()*10%3);
+
+			switch (empCheck){
+
+				case IS_FULL_TIME :
+					empHrs = 8;
+					break;
+
+				case IS_PART_TIME :
+					empHrs = 4;
+					break;
+
+				default:
+					empHrs = 0;
 			}
-		totalEmpHrs += empHrs;
-		System.out.println(" Day: " + totalWorkingDays);
-		System.out.println(" Dailly Employee Hour: " + empHrs);
-		System.out.println(" Total Employee hour: " + totalEmpHrs);
-		System.out.println(" ");
-		}		
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-		System.out.println(" Employee Wages For a Month: " + totalEmpWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day : "+totalWorkingDays + " Emp Hr : "+empHrs);
+
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHr;
+		System.out.println("Toatal Emp Wage for company : "+cmpName + " is : "+totalEmpWage);
+	}
+	public static void main(String [] args){
+
+		EmployeeWageComputation uc8 = new EmployeeWageComputation();
+		uc8.computeWage("D-mart",20,2,10);
+		uc8.computeWage("Relince",10,4,20);
 	}
 }
